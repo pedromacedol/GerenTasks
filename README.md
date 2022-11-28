4c0146cc2ac1ea502510c9cd4a1bede2690f8b55
pedromacedol <pedromacedoluna@gmail.com>
2022-11-28 01:42:05 -0300

    add readme

---
+++ README.md
@@ -0,0 +1,11 @@
+![Download New App (1)](https://user-images.githubusercontent.com/36522521/173712158-11ca3e6c-0b9a-4b99-93bc-6cd5123ed385.png)
+## TIC-TAC-TOE GAME 
+A project to pratice logic programming and python fundamentals.
+
+<img src="https://img.icons8.com/fluency/25/undefined/rules.png"/> <a href="">Rules</a>
+
+<img src="https://img.icons8.com/officel/23/undefined/system-task.png"/>  Game layout:
+
+![Captura de tela de 2022-06-14 21-53-48](https://user-images.githubusercontent.com/36522521/173714646-c84ae9b6-b7e9-45bb-80b6-8f51ee56bb95.png)
+![Captura de tela de 2022-06-14 22-07-32](https://user-images.githubusercontent.com/36522521/173714841-e2eec718-4dd3-449c-a889-b34aa01936ed.png)
+![Captura de tela de 2022-06-14 22-07-47](https://user-images.githubusercontent.com/36522521/173714847-33a1a796-d5a2-4464-9d04-97e379cad9d4.png)
--- script/app.js
+++ script/app.js
@@ -1,73 +1,80 @@
 //Array para armazenar as tarefas
 const tarefas = [];
 
+//Recebe "Dashboard" e define como oculto
 const dashboard = document.getElementById('Dashboard');
 dashboard.style.visibility = 'hidden';
 
+//Recebe "form" 
+const form = document.getElementById('form');
+
 
 // Função para adicionar uma nova tarefa ao array("tarefas")
-  function TarefaNova(event) {
-    var sizeTarefas = tarefas.length;
+function TarefaNova(event) {
 
-    var tarefa = document.getElementById("descricao").value;
-    tarefas.push(tarefa); 
+  //verifica o quantidade de tarefas existentes
+  var sizeTarefas = tarefas.length;
 
-    //Criando o box da tarefa adicionada.
-    const newDiv = document.createElement("div");
-    newDiv.setAttribute('value', sizeTarefas);
-    newDiv.setAttribute('class', 'box');
-    newDiv.setAttribute('id', ('div'+sizeTarefas));
+  //atribui a descrição da tarefa a var e adiciona ao array
+  var tarefa = document.getElementById("descricao").value;
+  tarefas.push(tarefa); 
 
-    const newTrash = document.createElement('img');
-    newTrash.setAttribute('src','https://img.icons8.com/ios-glyphs/30/null/delete.png');
+  //Criando um layout para exibir a tarefa na lista do "Dashboard"
+  const newDiv = document.createElement("div");
+  newDiv.setAttribute('value', sizeTarefas);
+  newDiv.setAttribute('class', 'box');
+  newDiv.setAttribute('id', ('div'+sizeTarefas));
+
+  //Definindo o descrição da tarefa
+  const newP = document.createElement("p");
+  newP.setAttribute('class', 'titulo');
+  newP.setAttribute('id', ('p'+sizeTarefas));
+  newP.innerText = tarefa;
+
+  //Div para organizer os botões
+  const buttonDiv = document.createElement("div");
+  buttonDiv.setAttribute('value', sizeTarefas);
+  buttonDiv.setAttribute('class', 'buttonsDiv');
+
+  //Definindo o botão "Concluir" e suas propiedades
+  const newButton = document.createElement('button');
+  newButton.setAttribute('class', 'botaoBox');
+  newButton.setAttribute('type', 'submit',);
+  newButton.setAttribute('onclick', 'Concluir(value, id)');
+  newButton.setAttribute('value', sizeTarefas);
+  newButton.setAttribute('id', ('concluirBotao'+sizeTarefas));
+  newButton.innerText = 'Concluir';
+
+  //Definindo o botão "Deletar" e suas propiedades
+  const delButton = document.createElement('button');
+  delButton.setAttribute('class', 'delete');
+  delButton.setAttribute('type', 'submit',);
+  delButton.setAttribute('onclick', 'Deletar(value, id)');
+  delButton.setAttribute('value', sizeTarefas);
+  delButton.setAttribute('id', ('delBotao'+sizeTarefas));
+  delButton.innerText = 'Deletar';
+
+  // inserindo os elementos criados a "Dashboard"
+  dashboard.appendChild(newDiv);
+  newDiv.appendChild(newP);
+  newDiv.append(buttonDiv);
+  buttonDiv.appendChild(delButton);
+  buttonDiv.appendChild(newButton);
 
 
-    //Definindo o descrição da tarefa
-    const newP = document.createElement("p");
-    newP.setAttribute('class', 'titulo');
-    newP.setAttribute('id', ('p'+sizeTarefas));
-    newP.innerText = tarefa;
+  // altera o valor do campo "descricao" para vazio novamento
+  document.getElementById("descricao").value = "";
 
-    //Div para organizer os botões
-    const buttonDiv = document.createElement("div");
-    buttonDiv.setAttribute('value', sizeTarefas);
-    buttonDiv.setAttribute('class', 'buttonsDiv');
-
-    //Definindo o botão "Concluir" e suas propiedades
-    const newButton = document.createElement('button');
-    newButton.setAttribute('class', 'botaoBox');
-    newButton.setAttribute('type', 'submit',);
-    newButton.setAttribute('onclick', 'Concluir(value, id)');
-    newButton.setAttribute('value', sizeTarefas);
-    newButton.setAttribute('id', ('concluirBotao'+sizeTarefas));
-    newButton.innerText = 'Concluir';
-
-    //Definindo o botão "Deletar" e suas propiedades
-    const delButton = document.createElement('button');
-    delButton.setAttribute('class', 'delete');
-    delButton.setAttribute('type', 'submit',);
-    delButton.setAttribute('onclick', 'Deletar(value, id)');
-    delButton.setAttribute('value', sizeTarefas);
-    delButton.setAttribute('id', ('delBotao'+sizeTarefas));
-    delButton.innerText = 'Deletar';
-
-
-    dashboard.appendChild(newDiv);
-    newDiv.appendChild(newP);
-    newDiv.append(buttonDiv);
-    buttonDiv.appendChild(delButton);
-    buttonDiv.appendChild(newButton);
-
-    document.getElementById("descricao").value = "";
-
-    event.preventDefault();
-  }
+  event.preventDefault();
+}
 
 function showDashboard() {
         dashboard.style.visibility = 'visible';
 }
 
+//Função para concluir uma "tarefa" específica
 function Concluir(posicao, id){
+
   child = document.getElementById(id);
 
   // Altera o estilo do título da tarefa
@@ -77,17 +84,20 @@ function Concluir(posicao, id){
   console.log(child);
 }
 
+//Função para excluir uma "tarefa" específica
 function Deletar(posicao, id){
+  //remove o elemento do array "tarefas"
   tarefas.splice(posicao, posicao+1); 
-  console.log(id)
+
+  //remove a "tarefa" da lista de exibição
   const child = document.getElementById(id);
   child.parentElement.parentElement.remove();
 }
 
-  const form = document.getElementById('form');
-
-  form.addEventListener('submit', showDashboard);
-  form.addEventListener('submit', TarefaNova);
+//eventos criados para exibir as tarefas na tela, após confirmar no botão.
+form.addEventListener('submit', TarefaNova);
+form.addEventListener('submit', showDashboard);
+  
  
 
   
